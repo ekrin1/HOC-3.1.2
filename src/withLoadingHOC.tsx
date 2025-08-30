5dd76409ae24d3b0ad681463554feacf63316655
ekrin1 <kottovakatya@mail.ru>
2025-08-30 12:43:30 +0300

    .

--- src/withLoadingHOC.tsx
+++ src/withLoadingHOC.tsx
@@ -1,1 +1,23 @@
-export function withLoading() {}
+import { Loader } from './Loader.tsx';
+import { ToDo } from './App';
+
+type withLoadingProps = {
+  isLoading: boolean;
+};
+
+type TodoListProps = {
+  todos: ToDo[];
+};
+
+export const withLoading = (Component: React.ComponentType<any>) => {
+  const EnhancedComponent = ({
+    isLoading,
+    ...props
+  }: withLoadingProps & TodoListProps) => {
+    if (isLoading) {
+      return <Loader />;
+    }
+    return <Component {...props} />;
+  };
+  return EnhancedComponent;
+};
